42725cdbe88cd7981c22c4f3f0fc4144f49e340d
LuH3ss <lucashess98@gmail.com>
2024-10-27 17:00:35 -0300

    data seeded

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -2,7 +2,10 @@ import bcrypt from 'bcrypt';
 import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-const client = await db.connect();
+
+
+  const client = await db.connect();
+
 
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -102,10 +105,10 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
   try {
     await client.sql`BEGIN`;
     await seedUsers();
